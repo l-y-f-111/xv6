f34e15d6441e7999f97c6d6e6951114b13c935b9
snake <1521164968@qq.com>
2023-01-05 00:07:35 +0800

    Lab: Xv6 and Unix utilities_pingpong

--- pingpong.c
+++ pingpong.c
@@ -14,14 +14,14 @@ int main(int argc,char *argv[]){
     pipe(fd_parent);
     pid = fork();
     if(pid==0){//child
-        read(fd_parent[0],buffer,sizeof(buffer));
+        read(fd_parent[0],buffer,sizeof(buffer));//child read from parent
         fprintf(2,"%d: received ping\n",getpid());
-        write(fd_child[1],buffer, sizeof(buffer));
+        write(fd_child[1],buffer, sizeof(buffer));//chirld write to parent
         exit(0);
     }
-    write(fd_parent[1],buffer, sizeof(buffer));
-    wait(0);
-    read(fd_child[0],buffer, sizeof(buffer));
+    write(fd_parent[1],buffer, sizeof(buffer));//parent write to child
+    wait(0);//parent wait child
+    read(fd_child[0],buffer, sizeof(buffer));//parent read from child
     fprintf(2,"%d: received pong\n",getpid());
     exit(0);
 }
